8eb6b6d606d415a583776539970fb459e18619b0
JasonHwang <junghahwang0601@gmail.com>
2024-10-07 17:07:06 +0900

    Feat: Header 초안

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import TestView from '@/components/TestView.vue'
+import { lectureRouter } from './lectureRouter';
+import LoginPage from '@/components/LoginPage.vue';
 const routes = [
     {
         path: '/test',
         name: 'TEST',
         component: TestView
-    }
+    },
+    {
+        path: '/login',
+        name: 'LOGIN',
+        component: LoginPage
+    },
+    ...lectureRouter
 ]
 const router = createRouter({
     history: createWebHistory(),

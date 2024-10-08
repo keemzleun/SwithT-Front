13038bb7f5a8c285e8d268bd344243b7c4a10ff3
Mincastle <kcm1042@naver.com>
2024-10-08 17:51:01 +0900

    feat:로그인 폼 작성하다 마무리

--- src/views/member/MemberEmailLoginView.vue
+++ src/views/member/MemberEmailLoginView.vue
@@ -1,7 +1,79 @@
 <template>
-    <h1>이메일 로그인화면</h1>
+  <v-container class="d-flex justify-center align-center" style="height: 65vh">
+    <v-card class="pa-16" outlined style="width: 800px; height: 500px">
+      <v-tabs v-model="activeTab" align-with-title>
+        <v-tab value="tutee">튜터 로그인</v-tab>
+        <v-tab value="tutor">튜티 로그인</v-tab>
+      </v-tabs>
+
+      <v-tabs-window v-model="activeTab">
+        <v-tabs-window-item value="tutee">
+            <v-card>
+                <br>
+                <br>
+                <v-form>
+                    <v-text-field
+                      label="이메일"
+                      v-model="tutorEmail"
+                      type="email"
+                      required
+                    ></v-text-field>
+                    <v-text-field
+                      label="비밀번호"
+                      v-model="tutorPassword"
+                      type="password"
+                      required
+                    ></v-text-field>
+                    <v-btn color="skyblue" type="submit" class="ma-3">
+                      튜터 로그인
+                    </v-btn>
+                  </v-form>
+            </v-card>
+        </v-tabs-window-item>
+      </v-tabs-window>
+
+      <v-tabs-window v-model="activeTab">
+        <v-tabs-window-item value="tutor">
+            <v-card>
+                <br>
+                <br>
+                <v-form>
+                    <v-text-field
+                      label="이메일"
+                      v-model="tutorEmail"
+                      type="email"
+                      required
+                    ></v-text-field>
+                    <v-text-field
+                      label="비밀번호"
+                      v-model="tutorPassword"
+                      type="password"
+                      required
+                    ></v-text-field>
+                    <v-btn color="skyblue" type="submit" class="ma-3">
+                      튜터 로그인
+                    </v-btn>
+                </v-form>
+            </v-card>
+        </v-tabs-window-item>
+      </v-tabs-window>
+    </v-card>
+  </v-container>
 </template>
+
 <script>
+export default {
+  data() {
+    return {
+      activeTab: 0,
+    };
+  },
+  methods: {},
+};
 </script>
-<style>
+
+<style scoped>
+.v-btn {
+  font-weight: bold;
+}
 </style>

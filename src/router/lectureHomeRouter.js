53405ac896fb6d575174e96df65591a8beac2a70
getsetgo1 <dnwls1313@gmail.com>
2024-10-10 10:46:33 +0900

    feat/lecture-home : 지도 api 검색 및 위도 경도 변환 후 지도 표시

--- src/router/lectureHomeRouter.js
+++ src/router/lectureHomeRouter.js
@@ -1,9 +1,15 @@
 import LectureHomeView from '@/views/lecture/home/LectureHomeView.vue'
+import MapTest from '@/views/lecture/home/MapTest.vue'
 export const lectureHomeRouter=[
     {
         path: '/lecture/:lectureGroupId',
         name: 'LectureHomeView',
         component: LectureHomeView
+    },
+    {
+        path: '/lecture/test',
+        name: 'MapTest',
+        component: MapTest
     },
 
 ]

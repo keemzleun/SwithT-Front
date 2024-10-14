8efcd18b233bbd1469e5de0d5794fedefb922c88
Mincastle <kcm1042@naver.com>
2024-10-14 09:13:35 +0900

    Feat: merge 하기전

--- src/views/member/MemberEmailLoginView.vue
+++ src/views/member/MemberEmailLoginView.vue
@@ -1,10 +1,8 @@
 <template>
   <v-container class="d-flex justify-center align-center" style="height: 65vh">
     <v-card 
-      class="pa-8" 
-      outlined 
-      style="background-color: ; width: 800px; height: 400px; border-radius: 25px">
-      
+    class="pa-8" 
+    outlined style="background-color: ; width: 800px; height: 400px; border-radius: 25px">
       <!-- 탭 메뉴 -->
       <v-tabs v-model="activeTab" background-color="#FFF490" grow centered>
         <v-tab style="font-weight: bold; color: black;">TUTOR 로그인</v-tab>
@@ -12,36 +10,35 @@
       </v-tabs>
 
       <v-tabs-window v-model="activeTab">
-        <v-tabs-window-item value="tutor">
-          <!-- 튜터 로그인 -->
-          <v-form @submit.prevent="doLogin" class="pa-4">
-            <v-text-field
-              label="이메일"
-              v-model="email"
-              type="email"
-              solo
-              required
-              hide-details
-            ></v-text-field>
-            <v-text-field
-              label="비밀번호"
-              v-model="password"
-              type="password"
-              solo
-              required
-              hide-details
-            ></v-text-field>
-
-            <v-btn color="#70b8ff" class="mx-auto mt-3" large style="width: 700px;" type="submit">
-              로그인
-            </v-btn>
-
-            <div class="text-center mt-3">
-              <a href="/member/email/register/tutor" class="no-underline">TUTOR 회원가입</a>
-            </div>
-          </v-form>
+        <v-tabs-window-item value="tutor" >
+        <!-- 튜터 로그인 -->
+        <v-form @submit.prevent="doLogin" class="pa-4">
+          <v-text-field
+            label="이메일"
+            v-model="email"
+            type="email"
+            solo
+            required
+            hide-details
+          ></v-text-field>
+          <v-text-field
+            label="비밀번호"
+            v-model="password"
+            type="password"
+            solo
+            required
+            hide-details
+          ></v-text-field>
+          
+          <v-btn color="#70b8ff" class="mx-auto mt-3" large style="width: 700px;" type="submit">
+            로그인
+          </v-btn>
+        </v-form>
+        <div class="text-center" style="margin-top: 20px;">
+          <a href="/member/email/register/tutor">TUTOR 회원가입</a>
+        </div>
         </v-tabs-window-item>
-        
+      
         <v-tabs-window-item>
           <!-- 튜티 로그인 -->
           <v-form @submit.prevent="doLogin" class="pa-4">
@@ -61,15 +58,13 @@
               required
               hide-details
             ></v-text-field>
-
-            <v-btn color="#70b8ff" class="mx-auto mt-3" large style="width: 700px;" type="submit">
+            <v-btn color="#70b8ff" block large class="ma-3" type="submit">
               로그인
             </v-btn>
-
-            <div class="text-center mt-3">
-              <a href="/member/email/register/tutee" class="no-underline">TUTEE 회원가입</a>
-            </div>
           </v-form>
+          <div class="text-center" style="margin-top: 20px;">
+            <a href="/member/email/register/tutee">TUTEE 회원가입</a>
+          </div>
         </v-tabs-window-item>
       </v-tabs-window>
     </v-card>
@@ -83,40 +78,48 @@ import { jwtDecode } from 'jwt-decode'
 export default {
   data() {
     return {
+      
       activeTab: 0, // 기본 탭을 튜터 로그인으로 설정
       email: "",
       password: ""
+      
     };
   },
   methods: {
     async doLogin() {
-      try {
-        const loginData = {
-          email: this.email,
-          password: this.password,
-        };
+            try {
 
-        const response = await axios.post(`${process.env.VUE_APP_API_BASE_URL}/member-service/doLogin`, loginData);
-        console.log("로그인 성공");
 
-        const token = response.data.result.token;
-        const refreshToken = response.data.result.refreshToken;
-        const role = jwtDecode(token).role;
-        const id = response.data.result.id;
+                const loginData = {
+                    email: this.email,
+                    password: this.password,
+                }
 
-        localStorage.setItem('token', token);
-        localStorage.setItem('refreshToken', refreshToken);
-        localStorage.setItem('role', role);
-        localStorage.setItem('id', id);
 
-        window.location.href = "/home";
+                const response = await axios.post(`${process.env.VUE_APP_API_BASE_URL}/member-service/doLogin`, loginData);
+                
+                console.log("로그인 성공");
+                
+                const token = response.data.result.token;
+                const refreshToken = response.data.result.refreshToken;
+                const role = jwtDecode(token).role;
+                const id = response.data.result.id;
 
-      } catch (e) {
-        const error_message = e.response.data.error_message;
-        console.error(error_message);
-        alert(error_message); // 추후 수정 할 것.
-      }
-    },
+                localStorage.setItem('token', token)
+                localStorage.setItem('refreshToken', refreshToken)
+                localStorage.setItem('role', role)
+                localStorage.setItem('id', id)
+            
+              
+
+                window.location.href = "/home"
+
+            } catch (e) {
+                const error_message = e.response.data.error_message
+                console.error(error_message);
+                alert(error_message); // 추후 수정 할 것.
+            }
+        },
   }
 };
 </script>
@@ -132,11 +135,5 @@ export default {
 a {
   color: black;
   font-weight: bold;
-}
-.no-underline {
-  text-decoration: none; /* 언더라인 제거 */
-}
-.no-underline:hover {
-  text-decoration: underline; /* 마우스 오버 시 언더라인 추가 */
 }
 </style>

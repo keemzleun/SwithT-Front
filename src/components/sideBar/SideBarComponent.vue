1a352e17c0048fa6cae0d7bb1e38d06c0e43624d
Mincastle <kcm1042@naver.com>
2024-11-01 22:59:06 +0900

    feat:쿠키 삭제 경로 설정 추가

--- src/components/sideBar/SideBarComponent.vue
+++ src/components/sideBar/SideBarComponent.vue
@@ -250,11 +250,14 @@
           this.$router.push("/member/main"); //김민성 수정
       },
       logout() {
-              // 모든 쿠키 삭제
-    document.cookie.split(";").forEach(cookie => {
-        const [name] = cookie.split("=");
-        document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=/;";
+        const paths = ['/', '/member/explain', '/loginSuccess']; // 지정된 경로들
+
+document.cookie.split(";").forEach(cookie => {
+    const [name] = cookie.split("=");
+    paths.forEach(path => {
+        document.cookie = `${name}=;expires=Thu, 01 Jan 1970 00:00:00 UTC;path=${path};domain=yourdomain.com`;
     });
+});
         
           localStorage.clear();
           

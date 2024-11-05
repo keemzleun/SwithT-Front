d946bad1f5d615cdb98189c8832afd9b22c8a040
getsetgo1 <dnwls1313@gmail.com>
2024-11-06 01:48:32 +0900

    fix : 강의상세 컴포넌트 tutor만 수정되게 처리

--- src/components/LectureDetailInfoComponent.vue
+++ src/components/LectureDetailInfoComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="top-info">
-        <v-row  class="align-center">
+        <v-row class="align-center">
             <v-col cols="7">
                 <div>
                     <span class="lectureType">
@@ -11,7 +11,10 @@
                     </span>
                 </div>
                 <div class="title">{{ lectureInfo?.title }}</div>
-                <div class="d-flex align-center"><span class="memberName"> {{ lectureInfo?.memberName }} 튜터</span><v-icon class="ml-1" style="font-size:20px" @click="gotoEdit()">mdi-pencil</v-icon></div>
+                <div class="d-flex align-center"><span class="memberName"> {{ lectureInfo?.memberName }}
+                        튜터</span><v-icon v-if="isTutor" class="ml-1" style="font-size:20px"
+                        @click="gotoEdit()">mdi-pencil</v-icon>
+                </div>
                 <div class="avgScore">
                     <span class="mdi mdi-star" style="font-size: 18px;"> </span>
                     <span style="font-size: 18px; margin-left: 3px;"> {{ lectureInfo?.avgScore }}점</span>
@@ -19,7 +22,7 @@
             </v-col>
             <v-col cols="5" class="d-flex align-center justify-center">
                 <div class="thumbnail">
-                    <img :src="lectureInfo?.image" alt="Lecture Thumbnail"/>
+                    <img :src="lectureInfo?.image" alt="Lecture Thumbnail" />
                 </div>
             </v-col>
         </v-row>
@@ -30,15 +33,17 @@
 import axios from 'axios';
 
 export default {
-    props:['lectureId'],
+    props: ['lectureId'],
     data() {
         return {
             lectureInfo: null, // 강의 정보를 저장할 객체
+            tutorName: null,
+            isTutor: false,
         };
     },
     computed: {
-        formattedLectureType(){
-            switch(this.lectureInfo?.lectureType) {
+        formattedLectureType() {
+            switch (this.lectureInfo?.lectureType) {
                 case 'LECTURE':
                     return '강의';
                 case 'LESSON':
@@ -71,14 +76,21 @@ export default {
             try {
                 const response = await axios.get(`${process.env.VUE_APP_API_BASE_URL}/lecture-service/lecture-detail/${this.lectureId}`);
                 this.lectureInfo = response.data.result; // 가져온 강의 정보를 저장
-                console.log(this.lectureInfo);
             } catch (error) {
                 console.error('강의 정보를 가져오는 데 실패했습니다:', error);
             }
+            this.checkTutor();
+
         },
-        gotoEdit(){
-            console.log()
+        gotoEdit() {
             this.$router.push(`/edit-lecture/${this.lectureId}`)
+        },
+        checkTutor() {
+            const token = localStorage.getItem('token');
+            if (token) {
+                this.tutorName = localStorage.getItem('name');
+            }
+            this.isTutor = this.tutorName === this.lectureInfo.memberName ? true : false;
         }
     },
 };
@@ -88,12 +100,14 @@ export default {
 .v-container {
     padding: 0 !important;
 }
+
 .top-info {
     background-color: #121212;
     color: white;
     padding: 100px 20vw 40px;
-    text-align: left; 
+    text-align: left;
 }
+
 .lectureType {
     display: inline-block;
     margin-right: 5px;
@@ -110,16 +124,21 @@ export default {
     font-weight: bold;
     word-wrap: break-word;
     word-break: break-all;
-    max-width: 80%; /* 제목의 최대 너비를 조절하기 위해 이 값을 설정 */
+    max-width: 80%;
+    /* 제목의 최대 너비를 조절하기 위해 이 값을 설정 */
     overflow-wrap: break-word;
-    white-space: normal; /* 제목 안에서 줄바꿈이 가능하도록 설정 */
+    white-space: normal;
+    /* 제목 안에서 줄바꿈이 가능하도록 설정 */
 }
+
 .memberName {
     font-size: 20px;
 }
+
 .avgScore {
     font-size: 1.5rem;
 }
+
 .thumbnail img {
     border-radius: 8px;
     height: 300px;
